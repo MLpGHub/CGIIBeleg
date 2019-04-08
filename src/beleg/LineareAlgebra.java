65044b8ca71d2ab12016d68e21a3604040a11df8
Emanuel Günther <eman.guenther@gmx.de>
2019-04-08 14:51:45 +0200

    init all

---
+++ src/beleg/LineareAlgebra.java
@@ -0,0 +1,142 @@
+package beleg;
+
+public class LineareAlgebra {
+
+	public static Vektor2D add(Vektor2D v1,Vektor2D v2) {
+			
+		
+	}
+	public static Vektor3D add(Vektor3D v1,Vektor3D v2) {
+		
+	}
+	public static void sub(Vektor2D v1,Vektor2D v2) {
+
+	}
+	public static void sub(Vektor3D v1,Vektor3D v2) {
+
+	}
+	public static void mult(Vektor2D v,double d) {
+
+	}
+	public static void mult(Vektor3D v,double d) {
+
+	}
+
+	public static void div(Vektor2D v,double d) {
+
+	}
+	public static void div(Vektor3D v,double d) {
+
+	}
+
+	public static void isEqual(Vektor2D v1,Vektor2D v2) {
+
+	}
+	public static void isEqual(Vektor3D v1,Vektor3D v2) {
+
+	}
+	
+	public static void isNotEqual(Vektor2D v1,Vektor2D v2) {
+
+	}
+	public static void isNotEqual(Vektor3D v1,Vektor3D v2) {
+
+	}
+
+	public static void length(Vektor2D v) {
+
+	}
+	public static void length(Vektor3D v) {
+
+	}
+
+	public static void normalize(Vektor2D v) {
+
+	}
+	public static void normalize(Vektor3D v) {
+
+	}
+	public static void euklDistance(Vektor2D v1,Vektor2D v2) {
+
+	}
+	public static void euklDistance(Vektor3D v1,Vektor3D v2) {
+
+	}
+
+	public static void manhattenDistance(Vektor2D v) {
+
+	}
+	public static void manhattenDistance(Vektor3D v) {
+
+	}
+
+
+	public static void crossProduct(Vektor3D v1,Vektor3D v2) {
+
+	}
+
+	public static void dotProduct(Vektor2D v1,Vektor2D v2) {
+
+	}
+	public static void dotProduct(Vektor3D v1,Vektor3D v2) {
+
+	}
+
+
+	public static void cosEquation(Vektor2D v1,Vektor2D v2) {
+
+	}
+	public static void cosEquation(Vektor3D v1,Vektor3D v2) {
+
+	}
+
+	public static void sinEquation(Vektor2D v1,Vektor2D v2) {
+
+	}
+	public static void sinEquation(Vektor3D v1,Vektor3D v2) {// der Sinus des Winkel
+
+	}
+
+	public static void angleRad(Vektor2D v1,Vektor2D v2) {// der Winkel in Rad
+
+	}
+	public static void angleRad(Vektor3D v1,Vektor3D v2) {// der Winkel in Rad
+
+	}
+
+	public static void angleDegree(Vektor2D v1,Vektor2D v2) {
+
+	}
+
+	public static void angleDegree(Vektor3D v1,Vektor3D v2) {
+
+	}
+
+	public static void radToDegree(double ang) {
+
+	}
+	public static void degreeToRad(double ang) {
+
+	}
+
+	public static void determinante(Vektor2D v1,Vektor2D v2) {
+
+	}
+	public static void determinante(Vektor3D v1,Vektor3D v2, Vektor3D v3) {
+
+	}
+
+	public static void abs(Vektor2D v) { // double ?
+
+	}
+
+	public static void show(Vektor2D v) {
+
+	}
+	public static void show(Vektor3D v) {
+
+	}
+
+
+
+}
